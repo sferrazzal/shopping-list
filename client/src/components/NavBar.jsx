97315b6de45f85e0d79be3f25244a583047363b1
sferrazzaf <francis.sferrazza@gmail.com>
2023-05-04 15:46:07 -0400

    Resize Navbar text and prevent collapse on small screens

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,19 +3,21 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand bg-body-tertiary">
             <div className="container-fluid">
-                <NavLink to="/" className="navbar-brand" href="#">Shopping List</NavLink>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink to="/lists" className="nav-link active" aria-current="page">Lists</NavLink>
+                            <NavLink to="/" className="nav-link active fs-3"  aria-current="page">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/recipes" className="nav-link active" aria-current="page">Recipes</NavLink>
+                            <NavLink to="/lists" className="nav-link active fs-3" aria-current="page">Lists</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/items" className="nav-link active" aria-current="page">Items</NavLink>
+                            <NavLink to="/recipes" className="nav-link active fs-3" aria-current="page">Recipes</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/items" className="nav-link active fs-3" aria-current="page">Items</NavLink>
                         </li>
 
                     </ul>
